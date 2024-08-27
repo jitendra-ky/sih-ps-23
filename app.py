9613c01a81176ea85b3a6463f3f78960cf6d1867
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-08-27 11:42:07 +0530

    changes made

---
+++ app.py
@@ -0,0 +1,21 @@
+from data import dt
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame(dt[1:], columns=dt[0])
+
+
+st.set_page_config(
+    page_title="SIH 2023 Problem-Statement",
+    page_icon="🪴",
+    layout="wide",
+)
+
+st.title("SIH 2023 Problem-Statement")
+
+st.write("This is a simple example of a Streamlit app that displays a table.")
+
+
+
+st.write(df)
+
