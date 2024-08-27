80990f19036d2909178c9d7cb657e47165c98756
zsquare12 <36557466+zsquare12@users.noreply.github.com>
2024-08-27 12:09:59 +0530

    my name branding added

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import streamlit as st
 
 df = pd.DataFrame(dt[1:], columns=dt[0])
 
-
 st.set_page_config(
     page_title="SIH 2023 Problem-Statement",
     page_icon="🪴",
     layout="wide",
 )
+st.write("Developed by [Jitendra-Kumar](https://www.jitendra.me)")
 
 st.title("SIH 2023 Problem-Statement")
 
